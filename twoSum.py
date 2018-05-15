a5c424fe3b3e40c8fd643b7c19dd4a4d0d2962ad
ericchai <ericchai923@gmail.com>
2018-05-16 00:25:44 +0800

    Update twoSum.py

--- twoSum.py
+++ twoSum.py
@@ -1,3 +1,4 @@
+# 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。你可以假设每个输入只对应一种答案，且同样的元素不能被重复利用。
 class Solution:
     def twoSum(self, nums, target):
         """
@@ -10,3 +11,14 @@ class Solution:
                 if num+second_num == target:
                     return list((j,k+j+1))
         return False
+    
+    
+    
+# 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。    
+class Solution:
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        return x >= 0 and str(x)[::1] == str(x)[::-1]
